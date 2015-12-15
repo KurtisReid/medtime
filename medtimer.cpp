210b4d4031444660c843e0fbbd78868e94f7539e
Kurtis Reid <kreid8@kent.edu>
2015-12-15 14:02:53 -0500

    changed structure data type to class for time

--- medtimer.cpp
+++ medtimer.cpp
@@ -9,24 +9,44 @@
 		input minute greater than 59 or less than 0 detection
 */
 #include <iostream>
+#include "time.h"
 using namespace std;
 
-void check_for_am_pm(int hour, int minute);
+//void check_for_am_pm(int hour, int minute);
 void patch_removal(int hour, int minute);
+void apply_patch_before(int hour, int minute);
 void patch_kick_in(int hour, int minute);
-void hour_check(int hour);
+//void hour_check(int hour);
 
-
+/*struct time
+{
+	int hour;
+	int minute;
+};*/
 
 
 
 
 int main()
 {
-	time applied;//time patch was applied
 	
+	time applied;//time patch was applied
+	int hour;
+	int minute;
+	cout << "please enter what hour patch was applied. " << endl;
+	cin >> hour;
+	cout << "please enter the minutes when patch was applied. " << endl;
+	cin >> minute;
+	applied.set_hour(hour);
+	applied.set_minute(minute);
+	applied.check_hour();
+	applied.check_minute();
+	applied.check_for_am_pm();
 	
 
+	patch_removal(applied.hour, applied.minute);
+	apply_patch_before(applied.hour, applied.minute);
+	patch_kick_in(applied.hour, applied.minute);
 	
 
 	
@@ -34,28 +54,41 @@ int main()
 	return 0;
 }
 
-time apply_patch(time applied)
+/*time apply_patch(time applied)
 {
 	//User inputs hour and minute patch was applied. Input is stored in struct and returned.
 	//Pre: time structure has hour
 	//Post: 
+	
+	int hour;
+	int minute;
 	cout << "please enter what hour patch was applied. " << endl;
-	cin >> applied.hour;
+	cin >> hour;
 	cout << "please enter the minutes when patch was applied. " << endl;
-	cin >> applied.minute;
+	cin >> minute;
+	
 	return applied;
-}
+}*/
 
-void check_for_am_pm(int hour, int minute)
+
+
+/*void check_for_am_pm(time applied)
 {
 	
-	if (hour > 12)
+	if (applied.hour > 12)
 	{
 		//checking for am pm
-		hour -= 12;
+		applied.hour -= 12;
+		
+		//check again
+		check_for_am_pm(applied);
 	}
 	
-}
+}*/
+
+
+
+
 void patch_removal(int hour, int minute)
 {
 	hour += 9;
@@ -68,12 +101,18 @@ void patch_kick_in(int hour, int minute)
 	hour += 2;
 	cout << "The patch willkick in at " << hour << ":" << minute << endl;
 }
+void apply_patch_before(int hour, int minute)
+{
+	//when to apply pacth for it to be effective by specified time
+	hour -= 2;
+	cout << "Apply patch before " << hour << ":" << minute << " for it to be effective";
+}
 
-void hour_check(int hour) 
+/*void hour_check(int hour) 
 {
 	if (hour > 12 || hour < 0)
 	{
 		cout << "Hour is not valid" << endl;
 	}
-}
+}*/
 
