7be21fbd9d3d07fc3d596283c7bb19c1626275b8
Kurtis Reid <kreid8@kent.edu>
2015-12-15 14:00:40 -0500

    added functions

--- time.h
+++ time.h
@@ -1,3 +1,36 @@
+/*
+	time(int hr, int min);
+	//constructor
+	
+	
+	
+	int get_hour() {return hour;}
+	//pre: hour has value
+	
+	
+	void set_hour(int new_hr){hour = new_hr;}
+	
+	
+	int get_minute(){return minute;}
+	
+	
+	void set_minute(int new_min){minute = new_min;}
+	
+	
+	void check_for_am_pm();
+		//Pre: 0 < hour < 24
+		
+		
+	void check_hour();
+	
+	
+	void check_minute();
+
+
+
+
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -6,16 +39,18 @@ class time
 {
 	public:
 		time(int hr, int min);
-		int get_hour();
-		void set_hour(int new_hr);
-		int get_minute();
-		void set_minute(int new_min);
+		int get_hour() {return hour;}
+		void set_hour(int new_hr){hour = new_hr;}
+		int get_minute(){return minute;}
+		void set_minute(int new_min){minute = new_min;}
 		void check_for_am_pm();
+		//Pre: 0 < hour < 24
 		void check_hour();
 		void check_minute();
 	
 	private:
 		int hour;
 		int minute;
+		string am_pm;
 	
 };
